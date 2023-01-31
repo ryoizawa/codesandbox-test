1f6f541abcd59d96c00bb8302605f263f83d7968
ryoizawa <101433518+ryoizawa@users.noreply.github.com>
2023-01-31 14:53:34 +0900

    Create reference_Emotion.jsx

---
+++ src/sample/reference_Emotion.jsx
@@ -0,0 +1,61 @@
+// App.js
+
+import "./styles.css";
+import { InlineStyle } from "./components/InlineStyle";
+import { CssModules } from "./components/CssModules";
+import { StyledJsx } from "./components/StyledJsx";
+import { StyledComponent } from "./components/StyledComponent";
+import { Emotion } from "./components/Emotion";
+
+export default function App() {
+  return (
+    <div className="App">
+      <InlineStyle />
+      <CssModules />
+      <StyledJsx />
+      <StyledComponent />
+      <Emotion />
+    </div>
+  );
+}
+
+
+// Emotion.jsx
+
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, css } from "@emotion/react";
+import { styled } from "@emotion/styled";
+export const Emotion = () => {
+  const containerStyle = css`
+    border: solid 2px blue;
+    border-radius: 20px;
+    padding: 8px;
+    margin: 8px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  `;
+  const titleStyle = css({
+    margin: 0,
+    color: "#3d84a8"
+  });
+  return (
+    <div css={containerStyle}>
+      <p css={titleStyle}>- Emotion -</p>
+      <SButton>FIGHT!</SButton>
+    </div>
+  );
+};
+
+const SButton = styled.button`
+  background-color: #abedd8;
+  border: none;
+  padding: 8px;
+  border-radius: 8px;
+  &:hover {
+    background-color: #46cdcf;
+    color: #fff;
+    cursor: pointer;
+  }
+`;
